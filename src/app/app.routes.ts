f3895f9cd972fb79bbbf52f1b2b026780fb91be6
VaishnaviYasala <yasalavaishnavi1301@gmail.com>
2025-11-20 10:38:19 -0500

    Added Login feature and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,33 @@
 import { Routes } from '@angular/router';
+import {SignupComponent} from "@app/app-auth/signup/signup.component";
+import {authGuard} from "@app/app-auth/auth.guard";
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: SignupComponent,
+  },
   {
     path: 'product',
     loadComponent: () => import('./product/cabinet-product/cabinet-product.component')
       .then(m => m.CabinetProductComponent),
     title: 'Dashboard',
+    canActivate: [authGuard],
   },
   {
     path: 'import',
     loadComponent: () => import('./product/csv-import/csv-import.component')
       .then(m => m.CsvImportComponent),
     title: 'Dashboard',
+    canActivate: [authGuard]
   },
   {
     path: 'list',
     loadComponent: () => import('./product/product-list/cabinet-product-list/cabinet-product-list.component')
       .then(m => m.CabinetProductListComponent),
     title: 'Dashboard',
+    canActivate: [authGuard]
   },
+  { path: '', redirectTo: '/product', pathMatch: 'full' },
+  { path: '**', redirectTo: '/product' },
 ];
